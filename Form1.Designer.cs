fcfff4471f4ff2939653b4f2f28081ee7eb3aa4d
RICARDO HERNANDEZ SALAS <rhernandezsalas24@gmail.com>
2025-09-21 18:33:17 -0600

    Graphic Changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -58,6 +58,8 @@
             this.textBox28 = new System.Windows.Forms.TextBox();
             this.textBox29 = new System.Windows.Forms.TextBox();
             this.textBox30 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -270,11 +272,32 @@
             this.textBox30.Size = new System.Drawing.Size(31, 26);
             this.textBox30.TabIndex = 25;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 41);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Validate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.lblTitle.Location = new System.Drawing.Point(163, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(417, 58);
+            this.lblTitle.TabIndex = 31;
+            this.lblTitle.Text = "Guess The Word!";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(710, 450);
+            this.ClientSize = new System.Drawing.Size(777, 504);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox26);
             this.Controls.Add(this.textBox27);
             this.Controls.Add(this.textBox28);
@@ -344,6 +367,8 @@
         private System.Windows.Forms.TextBox textBox28;
         private System.Windows.Forms.TextBox textBox29;
         private System.Windows.Forms.TextBox textBox30;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
 
